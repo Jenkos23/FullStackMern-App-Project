ec87253f38ae76a476ac5260e0f86fd7783345b1
Jenkos <you@example.com>
2024-12-17 08:39:35 -0800

    Added routes for user management with full CRUD capabilities

---
+++ routes/users.mjs
@@ -0,0 +1,82 @@
+import express from 'express';
+import User from '../Schema/model.mjs';
+import axios from 'axios';
+
+const router = express.Router();
+
+//CREATE -Adding a new user
+router.post('/register', async (req, res) =>{
+    const {name, email, password} = req.body;
+
+
+    try {
+
+
+        //Save to MongoDB
+        const newUser = new User({name, email, password});
+        await newUser.save();
+        res.status(201).json(newUser).send('User Created sucessfully');
+
+    } catch (error) {
+        res.status(400).json({message: error.message});
+        
+    }
+});
+
+
+//READ - Get all Users
+router.get ('/', async (req, res) => {
+    try {
+        const users = await User.find().limit(20);
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+});
+
+//READ - Get a user by ID
+router.get('/:id', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(user);
+    }catch (error){
+        res.status(500).json({message: error.message});
+
+    }
+});
+
+
+//UPDATE - Update a user by ID
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({message: error.messgae});
+        
+    }
+});
+
+//DELETE - Delete a user by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json({ message: 'User deleted' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+export default router;
